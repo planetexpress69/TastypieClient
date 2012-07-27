ba7ff84f1b55f4ce3bb68e67e10e5c52a6706109
Martin Kautz <martin@teambender.de>
2012-07-27 17:51:11 +0200

    Initial Commit

---
+++ TastypieClient/TBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TBAppDelegate.h
+//  TastypieClient
+//
+//  Created by Martin Kautz on 27.07.12.
+//  Copyright (c) 2012 Martin Kautz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TastypieClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TastypieClient
+//
+//  Created by Martin Kautz on 27.07.12.
+//  Copyright (c) 2012 Martin Kautz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBAppDelegate class]));
+    }
+}
